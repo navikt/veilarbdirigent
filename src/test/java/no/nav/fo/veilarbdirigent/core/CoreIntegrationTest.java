260bf450222594f1bbfaafd22ef207fb44e6ee12
alexander-svendsen <alexander.svendsen@nav.no>
2018-03-14 14:47:37 +0100

    FO-584: Refaktorer core integrasjons tester

--- src/test/java/no/nav/fo/veilarbdirigent/core/CoreIntegrationTest.java
+++ src/test/java/no/nav/fo/veilarbdirigent/core/CoreIntegrationTest.java
@@ -2,27 +2,28 @@ package no.nav.fo.veilarbdirigent.core;
 
 import io.vavr.collection.List;
 import no.nav.fo.veilarbdirigent.TestUtils;
-import no.nav.fo.veilarbdirigent.config.*;
+import no.nav.fo.veilarbdirigent.config.CoreConfig;
+import no.nav.fo.veilarbdirigent.config.DAOConfig;
+import no.nav.fo.veilarbdirigent.config.DbConfig;
 import no.nav.fo.veilarbdirigent.coreapi.Actuator;
 import no.nav.fo.veilarbdirigent.coreapi.MessageHandler;
 import no.nav.fo.veilarbdirigent.coreapi.Status;
 import no.nav.fo.veilarbdirigent.coreapi.Task;
 import no.nav.fo.veilarbdirigent.dao.TaskDAO;
 import no.nav.fo.veilarbdirigent.db.AbstractIntegrationTest;
-import no.nav.fo.veilarbdirigent.db.DatabaseTestContext;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+import org.springframework.context.annotation.Bean;
 
 import javax.naming.NamingException;
 
 import static no.nav.fo.veilarbdirigent.TestUtils.delay;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CoreIntegrationTest extends AbstractIntegrationTest {
 
@@ -40,28 +41,28 @@ class CoreIntegrationTest extends AbstractIntegrationTest {
 
     @BeforeAll
     @BeforeClass
-    public static void setupContext() throws NamingException {
-        DatabaseTestContext.setupInMemoryContext();
+    static void setupContext() throws NamingException {
         setupContext(
                 CoreConfig.class,
                 DbConfig.class,
                 DAOConfig.class,
-                MessageHandlerTestConfig.class,
-                ActuatorTestConfig.class
+                CoreIntegrationTest.TestConfig.class
         );
     }
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         when(messageHandler.handle(any())).thenReturn(TASKS);
     }
 
     @Test
-    void name() {
-        given_actuator_error_on_task(2);
+    void core_should_transform_messages_to_tasks_save_them_and_execute_them() {
+
+        throwExpectionOnTaskWithId(TASKS.get(2).getId());
         core.submit(null);
 
-        assertThat(dao.fetchTasks().length()).isEqualTo(4);
+        List<Task> savedTasks = dao.fetchTasks();
+        assertThat(savedTasks.length()).isEqualTo(4);
 
         delay(200);
 
@@ -71,7 +72,28 @@ class CoreIntegrationTest extends AbstractIntegrationTest {
         assertThat(nonCompletedTasks.get(0).getStatus()).isEqualByComparingTo(Status.FAILED);
     }
 
-    private void given_actuator_error_on_task(int failed) {
-        when(actuator.handle(TASKS.get(failed))).thenThrow(new RuntimeException());
+    @SuppressWarnings("unchecked")
+    private void throwExpectionOnTaskWithId(String taskId) {
+        when(actuator.handle(any())).thenAnswer(a -> {
+            Task task = (Task) a.getArguments()[0];
+            if (taskId.equals(task.getId())) {
+                throw new RuntimeException();
+            }
+            return task;
+        });
+    }
+
+    static class TestConfig {
+        @Bean
+        public Actuator actuator() {
+            Actuator mock = mock(Actuator.class);
+            when(mock.getType()).thenReturn(TestUtils.TASK_TYPE);
+            return mock;
+        }
+
+        @Bean
+        public MessageHandler messageHandler() {
+            return mock(MessageHandler.class);
+        }
     }
 }

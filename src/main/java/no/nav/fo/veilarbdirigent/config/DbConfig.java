ec5ab93131a5ddc04e09ea55099a0ccbc23a5ada
alexander-svendsen <alexander.svendsen@nav.no>
2018-03-15 10:42:22 +0100

    KAIZEN-0: Bruk ny db-config for NAIS

--- src/main/java/no/nav/fo/veilarbdirigent/config/DbConfig.java
+++ src/main/java/no/nav/fo/veilarbdirigent/config/DbConfig.java
@@ -1,28 +1,40 @@
 package no.nav.fo.veilarbdirigent.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import no.nav.apiapp.selftest.HelsesjekkMetadata;
 import no.nav.sbl.dialogarena.types.Pingable;
-import no.nav.sbl.jdbc.Transactor;
 import no.nav.sbl.sql.SqlUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
+
 @Configuration
 @EnableTransactionManagement
 public class DbConfig {
 
+    public static final String VEILARBJOBBSOKERKOMPETANSEDB_URL = "VEILARBDIRIGENTDB_URL";
+    public static final String VEILARBJOBBSOKERKOMPETANSEDB_USERNAME = "VEILARBDIRIGENTDB_USERNAME";
+    public static final String VEILARBJOBBSOKERKOMPETANSEDB_PASSWORD = "VEILARBDIRIGENTDB_PASSWORD";
+
     @Bean
-    public DataSource dataSourceJndiLookup() throws NamingException {
-        JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
-        jndiDataSourceLookup.setResourceRef(true);
-        return jndiDataSourceLookup.getDataSource("jdbc/veilarbdirigentDB");
+    public static DataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(getRequiredProperty(VEILARBJOBBSOKERKOMPETANSEDB_URL));
+        config.setUsername(getRequiredProperty(VEILARBJOBBSOKERKOMPETANSEDB_USERNAME));
+        config.setPassword(getRequiredProperty(VEILARBJOBBSOKERKOMPETANSEDB_PASSWORD));
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+
+        return new HikariDataSource(config);
     }
 
     @Bean
@@ -36,17 +48,11 @@ public class DbConfig {
     }
 
     @Bean
-    public Transactor transactor(PlatformTransactionManager platformTransactionManager) {
-        return new Transactor(platformTransactionManager);
-    }
-
-    @Bean
     public Pingable dbPinger(final DataSource ds) {
-        Pingable.Ping.PingMetadata metadata = new Pingable.Ping.PingMetadata(
-                "veilarboppfolgingDB: " + System.getProperty("veilarboppfolgingDB.url"),
-                "Enkel spørring mot Databasen for VeilArbOppfolging.",
-                true
-        );
+        HelsesjekkMetadata metadata = new HelsesjekkMetadata("db",
+                "Database: " + getRequiredProperty(VEILARBJOBBSOKERKOMPETANSEDB_URL),
+                "Database for veilarbdirigent",
+                true);
 
         return () -> {
             try {

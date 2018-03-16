0c32a5a1885214876174f3599b9de2f012c01531
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-03-16 13:49:33 +0100

    [FO-584] Initielt forslag til lasting av taskdefinisjoner fra fil

---
+++ src/main/java/no/nav/fo/veilarbdirigent/core/TaskRepository.java
@@ -0,0 +1,80 @@
+package no.nav.fo.veilarbdirigent.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.Tuple;
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+@Slf4j
+public class TaskRepository {
+    private static Map<String, String> taskcontent;
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static <T> Option<T> get(String name, Class<T> type) {
+        if (taskcontent == null) {
+            taskcontent = readAllFiles();
+        }
+
+        return taskcontent
+                .get(name)
+                .onEmpty(() -> log.warn("Requested '" + name + "', but taskdefinition does not exist."))
+                .flatMap((content) -> readvalue(content, type).toOption());
+    }
+
+    private static <T> Either<Exception, T> readvalue(String content, Class<T> type) {
+        try {
+            return Either.right(mapper.readValue(content, type));
+        } catch (Exception e) {
+            log.error("Parsing content", e);
+            return Either.left(e);
+        }
+    }
+
+    private static Map<String, String> readAllFiles() {
+        return Option.of(TaskRepository.class.getClassLoader().getResource("taskdefinitions"))
+                .map(URL::getFile)
+                .map(File::new)
+                .map(TaskRepository::listFiles)
+                .getOrElse(List.empty())
+                .map((file) -> Tuple.of(findName(file), readFile(file).getOrElse((String) null)))
+                .toMap(Function.identity());
+    }
+
+    private static String findName(File file) {
+        String filenameWithExtension = file.getName();
+        int extensionIndex = filenameWithExtension.lastIndexOf(".");
+
+        return filenameWithExtension.substring(0, extensionIndex);
+    }
+
+    private static Either<Exception, String> readFile(File file) {
+        try {
+            return Either.right(new String(Files.readAllBytes(Paths.get(file.getAbsolutePath()))));
+        } catch (Exception e) {
+            log.error("Reading file", e);
+            return Either.left(e);
+        }
+    }
+
+    private static List<File> listFiles(File directory) {
+        return Option.of(directory.listFiles())
+                .map(List::of)
+                .getOrElse(List.empty())
+                .flatMap((File file) -> {
+                    if (file.isFile()) {
+                        return List.of(file);
+                    } else {
+                        return listFiles(directory);
+                    }
+                });
+    }
+}
---
+++ src/test/java/no/nav/fo/veilarbdirigent/core/TaskRepositoryTest.java
@@ -0,0 +1,42 @@
+package no.nav.fo.veilarbdirigent.core;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+class TaskRepositoryTest {
+    @Test
+    void should_read_file_into_class() {
+        Option<AktivitetTestData> cvAktivitet = TaskRepository.get("test_cv_aktivitet", AktivitetTestData.class);
+
+        assertThat(cvAktivitet.isSingleValued()).isTrue();
+        assertThat(cvAktivitet.get().type).isEqualTo("TEST");
+        assertThat(cvAktivitet.get().tittel).isEqualTo("Tittel");
+        assertThat(cvAktivitet.get().hensikt).isEqualTo("Hensikt");
+    }
+
+    @Test
+    void should_give_option_none_if_it_doesnt_exist() {
+        Option<AktivitetTestData> cvAktivitet = TaskRepository.get("fake-task", AktivitetTestData.class);
+
+        assertThat(cvAktivitet.isEmpty()).isTrue();
+    }
+
+    @Test
+    void should_give_option_none_if_marshalling_fails() {
+        Option<String> cvAktivitet = TaskRepository.get("test_cv_aktivitet", String.class);
+
+        assertThat(cvAktivitet.isEmpty()).isTrue();
+    }
+
+    // NB, be careful with using lombok on POJOs meant for jackson
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AktivitetTestData {
+        public String type;
+        public String tittel;
+        public String hensikt;
+    }
+}

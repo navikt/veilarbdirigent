2476a5924804d954a72ec6efdaed98524a99bb8b
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-03-19 12:09:57 +0100

    [FO-584] Samlet TimeUtils

--- src/main/java/no/nav/fo/veilarbdirigent/utils/TimeUtils.java
+++ src/main/java/no/nav/fo/veilarbdirigent/utils/TimeUtils.java
@@ -1,8 +1,50 @@
 package no.nav.fo.veilarbdirigent.utils;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TimeUtils {
+    private static final String MONTHS = "m";
+    private static final String WEEKS = "u";
+    private static final String DAYS = "d";
+    private static final String HOURS = "t";
+
+    private static Pattern pattern = Pattern.compile("(\\d+["+MONTHS+WEEKS + DAYS + HOURS+"])");
+
+    static LocalDateTime relativeTime(LocalDateTime now, String relative) {
+        LocalDateTime time = now;
+        Matcher matcher = pattern.matcher(relative);
+
+        while (matcher.find()) {
+            time = createTimeFunction(matcher.group()).apply(time);
+        }
+
+        return time;
+    }
+
+    public static LocalDateTime relativeTime(String relative) {
+        return relativeTime(LocalDateTime.now(), relative);
+    }
+
+    private static Function<LocalDateTime, LocalDateTime> createTimeFunction(String time) {
+        return (date) -> {
+            int number = Integer.parseInt(time.substring(0, time.length() - 1), 10);
+            if (time.endsWith(MONTHS)) {
+                return date.plusMonths(number);
+            } else if (time.endsWith(WEEKS)) {
+                return date.plusWeeks(number);
+            } else if (time.endsWith(DAYS)) {
+                return date.plusDays(number);
+            } else if (time.endsWith(HOURS)) {
+                return date.plusHours(number);
+            } else {
+                return date;
+            }
+        };
+    }
+
     public static LocalDateTime exponentialBackoff(int attempts, LocalDateTime now) {
         long secondsToWait = (long)Math.pow(2, attempts);
         return now.plusSeconds(secondsToWait);
--- src/test/java/no/nav/fo/veilarbdirigent/utils/TimeUtilsTest.java
+++ src/test/java/no/nav/fo/veilarbdirigent/utils/TimeUtilsTest.java
@@ -1,43 +1,42 @@
 package no.nav.fo.veilarbdirigent.utils;
 
-import no.nav.fo.veilarbdirigent.utils.ActuatorUtils;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-class ActuatorUtilsTest {
+class TimeUtilsTest {
     static LocalDateTime now = LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0);
 
     @Test
     void plus_hours() {
-        LocalDateTime future = ActuatorUtils.relativeTime(now, "12t");
+        LocalDateTime future = TimeUtils.relativeTime(now, "12t");
         assertThat(future).isEqualByComparingTo(LocalDateTime.of(1970, 1, 1, 12, 0));
     }
 
     @Test
     void plus_days() {
-        LocalDateTime future = ActuatorUtils.relativeTime(now, "5d");
+        LocalDateTime future = TimeUtils.relativeTime(now, "5d");
         assertThat(future).isEqualByComparingTo(LocalDateTime.of(1970, 1, 6, 0, 0));
     }
 
     @Test
     void plus_weeks() {
-        LocalDateTime future = ActuatorUtils.relativeTime(now, "5u");
+        LocalDateTime future = TimeUtils.relativeTime(now, "5u");
         assertThat(future).isEqualByComparingTo(LocalDateTime.of(1970, 2, 5, 0, 0));
     }
 
     @Test
     void plus_months() {
-        LocalDateTime future = ActuatorUtils.relativeTime(now, "2m");
+        LocalDateTime future = TimeUtils.relativeTime(now, "2m");
         assertThat(future).isEqualByComparingTo(LocalDateTime.of(1970, 3, 1, 0, 0));
     }
 
     @Test
     void plus_combination() {
-        LocalDateTime future = ActuatorUtils.relativeTime(now, "2m5u5d12t");
-        LocalDateTime future2 = ActuatorUtils.relativeTime(now, "2m 5u 5d 12t");
+        LocalDateTime future = TimeUtils.relativeTime(now, "2m5u5d12t");
+        LocalDateTime future2 = TimeUtils.relativeTime(now, "2m 5u 5d 12t");
 
         LocalDateTime expected = LocalDateTime.of(1970, 4, 10, 12, 0);
         assertThat(future).isEqualByComparingTo(expected);

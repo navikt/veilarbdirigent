3e82bcd50603aef72d450cf70760b000a48bcf62
alexander-svendsen <alexander.svendsen@nav.no>
2018-03-19 15:20:35 +0100

    KAIZEN-0: Legg til isso sikkerhet på appen

--- src/main/java/no/nav/fo/veilarbdirigent/config/ApplicationConfig.java
+++ src/main/java/no/nav/fo/veilarbdirigent/config/ApplicationConfig.java
@@ -43,6 +43,7 @@ public class ApplicationConfig implements ApiApplication.NaisApiApplication {
 
     @Override
     public void configure(ApiAppConfigurator apiAppConfigurator) {
+        apiAppConfigurator.issoLogin().sts();
 
     }
 }

32eed6234b26a53ffb6c3d2f2c8b92789dbce373
alexander-svendsen <alexander.svendsen@nav.no>
2018-03-19 15:44:41 +0100

    KAIZEN-0: Legg til securityTokenService i main test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,6 +2,7 @@ import no.nav.brukerdialog.tools.SecurityConstants;
 import no.nav.dialogarena.config.fasit.FasitUtils;
 import no.nav.dialogarena.config.fasit.ServiceUser;
 import no.nav.fo.veilarbdirigent.config.DatabaseTestContext;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 import no.nav.testconfig.ApiAppTest;
 
 import static java.lang.System.getProperty;
@@ -17,7 +18,10 @@ public class MainTest {
         ApiAppTest.setupTestContext();
         DatabaseTestContext.setupContext(getProperty("database"));
 
+        String securityTokenService = FasitUtils.getBaseUrl("securityTokenService");
         ServiceUser srvveilarbdirigent = FasitUtils.getServiceUser("srvveilarbdirigent", APPLICATION_NAME);
+
+        setProperty(StsSecurityConstants.STS_URL_KEY, securityTokenService);
 
         setProperty(SecurityConstants.SYSTEMUSER_USERNAME, srvveilarbdirigent.getUsername());
         setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, srvveilarbdirigent.getPassword());

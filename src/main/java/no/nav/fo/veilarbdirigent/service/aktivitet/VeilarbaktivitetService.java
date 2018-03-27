b541fe9ee281e5695e4bbb51df5a26f24ad739bd
alexander-svendsen <alexander.svendsen@nav.no>
2018-03-27 18:04:11 +0200

    KAIZEN-0: Flytt Client bean og lag test oppsett
    Ønsker ikke å sette opp sikkerheten i integrasjons test oppsettet
    så fjerner denne delen av koden

--- src/main/java/no/nav/fo/veilarbdirigent/service/aktivitet/VeilarbaktivitetService.java
+++ src/main/java/no/nav/fo/veilarbdirigent/service/aktivitet/VeilarbaktivitetService.java
@@ -1,15 +1,15 @@
 package no.nav.fo.veilarbdirigent.service.aktivitet;
 
 import io.vavr.control.Try;
-import no.nav.brukerdialog.security.oidc.SystemUserTokenProvider;
 import no.nav.fo.veilarbaktivitet.domain.AktivitetDTO;
-import no.nav.sbl.rest.RestUtils;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.client.*;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
@@ -18,23 +18,11 @@ public class VeilarbaktivitetService {
     public static final String VEILARBAKTIVITETAPI_URL_PROPERTY = "VEILARBAKTIVITETAPI_URL";
     private final String host;
 
+    @Inject
+    private Client client;
+
     public VeilarbaktivitetService() {
         this.host = getRequiredProperty(VEILARBAKTIVITETAPI_URL_PROPERTY);
-    }
-
-    public Client client() {
-        Client client = RestUtils.createClient();
-        client.register(new SystemUserOidcTokenProviderFilter());
-        return client;
-    }
-
-    private static class SystemUserOidcTokenProviderFilter implements ClientRequestFilter {
-        private SystemUserTokenProvider systemUserTokenProvider = new SystemUserTokenProvider();
-
-        @Override
-        public void filter(ClientRequestContext clientRequestContext) throws IOException {
-            clientRequestContext.getHeaders().putSingle("Authorization", "Bearer " + systemUserTokenProvider.getToken());
-        }
     }
 
     public static class VeilArbAktivitetServiceException extends Exception {
@@ -45,7 +33,7 @@ public class VeilarbaktivitetService {
 
     public Try<AktivitetDTO> lagAktivitet(String aktorId, AktivitetDTO data) {
         String url = String.format("%s/aktivitet/ny?aktorId=%s", host, aktorId);
-        Invocation.Builder request = client().target(url).request();
+        Invocation.Builder request = client.target(url).request();
         Response post = request.post(Entity.entity(data, MediaType.APPLICATION_JSON));
 
         if (post.getStatus() >= 200 && post.getStatus() < 300) {

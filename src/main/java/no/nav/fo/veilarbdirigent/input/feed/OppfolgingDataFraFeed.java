837da2b1034fde22de8ba3724966698fe8d9dd41
Joselito Poblete <joselito.poblete@gmail.com>
2018-11-19 16:56:41 +0100

    FO-1633 Sykmeldt over 39 uker som registrer seg skal få automatisk dialog og aktiviteter

--- src/main/java/no/nav/fo/veilarbdirigent/input/feed/OppfolgingDataFraFeed.java
+++ src/main/java/no/nav/fo/veilarbdirigent/input/feed/OppfolgingDataFraFeed.java
@@ -19,6 +19,7 @@ public class OppfolgingDataFraFeed implements Message, Comparable<OppfolgingData
     String aktorId;
     String foreslattInnsatsgruppe;
     Date opprettet;
+    String brukergruppe;
 
     @Override
     public int compareTo(OppfolgingDataFraFeed o) {

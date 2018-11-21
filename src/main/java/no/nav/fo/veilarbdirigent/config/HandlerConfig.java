4d221be6149000753e45fe595d16058bc9230ce1
alexander-svendsen <alexander.svendsen@nav.no>
2018-11-21 12:39:47 +0100

    KAIZEN-0: Rename oppHandler -> aktivitetHandler

--- src/main/java/no/nav/fo/veilarbdirigent/config/HandlerConfig.java
+++ src/main/java/no/nav/fo/veilarbdirigent/config/HandlerConfig.java
@@ -1,7 +1,7 @@
 package no.nav.fo.veilarbdirigent.config;
 
+import no.nav.fo.veilarbdirigent.handlers.AktivitetHandler;
 import no.nav.fo.veilarbdirigent.handlers.BusyHandler;
-import no.nav.fo.veilarbdirigent.handlers.OppfolgingsHandler;
 import no.nav.fo.veilarbdirigent.handlers.DialogHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HandlerConfig {
     @Bean
-    public OppfolgingsHandler oppfolgingsHandler() {
-        return new OppfolgingsHandler();
+    public AktivitetHandler oppfolgingsHandler() {
+        return new AktivitetHandler();
     }
 
     @Bean

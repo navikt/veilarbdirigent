548eaf121e6c9897d56211ac9548670d025f6f6e
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-03-17 13:28:22 +0100

    FO-2802: Legg til auto dialog ved permittering

--- src/main/java/no/nav/fo/veilarbdirigent/handlers/DialogHandler.java
+++ src/main/java/no/nav/fo/veilarbdirigent/handlers/DialogHandler.java
@@ -1,67 +1,46 @@
 package no.nav.fo.veilarbdirigent.handlers;
 
-import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
-import io.vavr.collection.Map;
+import io.vavr.control.Option;
 import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import no.nav.fo.veilarbdirigent.core.Core;
 import no.nav.fo.veilarbdirigent.core.api.*;
 import no.nav.fo.veilarbdirigent.input.feed.OppfolgingDataFraFeed;
+import no.nav.fo.veilarbdirigent.output.domain.Besvarelse;
+import no.nav.fo.veilarbdirigent.output.domain.DinSituasjonSvar;
+import no.nav.fo.veilarbdirigent.output.domain.OrdinaerBrukerRegistrering;
 import no.nav.fo.veilarbdirigent.output.services.VeilarbdialogService;
+import no.nav.fo.veilarbdirigent.output.services.VeilarbregisteringService;
 import no.nav.fo.veilarbdirigent.utils.TypedField;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Optional;
 
 public class DialogHandler implements MessageHandler, Actuator<DialogHandler.OppfolgingData, String> {
     private final TaskType TYPE = TaskType.of("OPPFOLGING_OPPRETT_DIALOG");
 
     @Inject
     Core core;
+
+    @Inject
+    private VeilarbregisteringService veilarbregisteringService;
+
     @Inject
     private VeilarbdialogService service;
 
-    private final List<String> sykmeldtBrukerTyper = List.of(
-            "SKAL_TIL_NY_ARBEIDSGIVER",
-            "SKAL_TIL_SAMME_ARBEIDSGIVER"
-    );
+    private static final String permitertDialog = "kanskje_permitert_dialog";
 
-    private static final String behovForArbeidsevnevurdering = "BEHOV_FOR_ARBEIDSEVNEVURDERING";
-    private static final String sykemeldtDialog = "sykemeldt_dialog";
-    private static final String arbeidsevnevurderingDialog = "arbeidsevnevurdering_dialog";
-
-
-    private static final String sykemeldtJson = "{\n" +
-            "\"overskrift\": \"Mer veiledning fra NAV\",\n" +
+    private static final String permitertJson = "{\n" +
+            "\"overskrift\": \"PERM!!\",\n" +
             "\"tekst\": \"Hei!\\n" +
-            "Du har svart at du trenger mer veiledning nå som retten til sykepenger nærmer seg slutten. Vi vil veilede deg videre og trenger derfor å vite litt mer.\\n" +
-            "Du kan velge om du vil fortelle om situasjonen din \\n" +
-            "- i et møte med veilederen din på NAV-kontoret\\n" +
-            "- i en telefonsamtale\\n" +
-            "- her i dialogen\\n" +
-            "Skriv svaret ditt i feltet over. Hvis du velger \\\"her i dialogen\\\", kan du fortelle mer allerede nå.\"\n" +
+            "Nå har du fri en stund \\n" +
             "}";
 
-    private static final String arbeidsevnevurderingJson = "{\n" +
-            "\"overskrift\": \"Mer veiledning fra NAV\",\n" +
-            "\"tekst\": \"Hei!\\n" +
-            "Du har svart at du har utfordringer som hindrer deg i å søke eller være i jobb. Vi vil veilede deg videre og trenger derfor å vite litt mer.\\n" +
-            "Du kan velge om du vil fortelle om situasjonen din \\n" +
-            "- i et møte med veilederen din på NAV-kontoret\\n" +
-            "- i en telefonsamtale\\n" +
-            "- her i dialogen\\n" +
-            "Skriv svaret ditt i feltet over. Hvis du velger \\\"her i dialogen\\\", kan du fortelle mer allerede nå.\"\n" +
-            "}";
-
-    private static final HashMap<String, String> meldinger = HashMap.ofEntries(
-            Map.entry(sykemeldtDialog, sykemeldtJson),
-            Map.entry(arbeidsevnevurderingDialog, arbeidsevnevurderingJson)
-    );
+    private final List<String> registeringForslag = List.of("STANDARD_INNSATS",
+            "SITUASJONSBESTEMT_INNSATS",
+            "BEHOV_FOR_ARBEIDSEVNEVURDERING");
 
 
     @PostConstruct
@@ -75,55 +54,40 @@ public class DialogHandler implements MessageHandler, Actuator<DialogHandler.Opp
         if (message instanceof OppfolgingDataFraFeed) {
             OppfolgingDataFraFeed msg = (OppfolgingDataFraFeed) message;
 
-
-            // creating a Calendar object
-            Calendar c = Calendar.getInstance();
-            c.set(2020, Calendar.MARCH, 12, 0, 0, 0);
-            Date turnOfDate = c.getTime();
-
-            if (Optional.ofNullable(msg.getOpprettet()).map(d -> d.after(turnOfDate)).orElse(true)){
-                return List.empty();
-            }
-
-            boolean erNySykmeldtBrukerRegistrert = sykmeldtBrukerTyper.contains(msg.getSykmeldtBrukerType());
-
-            if (erNySykmeldtBrukerRegistrert) {
+            boolean erNyRegistrert = registeringForslag.contains(msg.getForeslattInnsatsgruppe());
+            if (erNyRegistrert) {
                 return List.of(
                         new Task<>()
-                                .withId(String.valueOf(msg.getId()) + "sykemeldtDialog")
+                                .withId(String.valueOf(msg.getId()) + "kanskjePermitert")
                                 .withType(TYPE)
-                                .withData(new TypedField<>(new OppfolgingData(msg, sykemeldtDialog))));
-            } else if (behovForArbeidsevnevurdering.equals(msg.getForeslattInnsatsgruppe())) {
-                return List.of(
-                        new Task<>()
-                                .withId(String.valueOf(msg.getId()) + "arbeidsevneDialog")
-                                .withType(TYPE)
-                                .withData(new TypedField<>(new OppfolgingData(msg, arbeidsevnevurderingDialog))));
+                                .withData(new TypedField<>(new OppfolgingData(msg, permitertDialog))));
             }
-
-            return List.empty();
-        } else {
-            return List.empty();
         }
+        return List.empty();
     }
 
 
     @Override
     public Try<String> handle(DialogHandler.OppfolgingData data) {
-        String dialogJson = meldinger
-                .get(data.meldingsName)
-                .getOrElse(sykemeldtJson);
 
-        return service.lagDialog(data.feedelement.getAktorId(), dialogJson);
+        Option<DinSituasjonSvar> svar = veilarbregisteringService
+                .hentRegistrering(data.feedelement.getAktorId())
+                .toOption()
+                .flatMap(Option::of)
+                .map(OrdinaerBrukerRegistrering::getBesvarelse)
+                .map(Besvarelse::getDinSituasjon);
+
+
+        if(svar.map(DinSituasjonSvar.ER_PERMITTERT::equals).getOrElse(false)){
+            return service.lagDialog(data.feedelement.getAktorId(), permitertJson);
+        }
+
+        return Try.success("Nothing to do here");
     }
 
     @NoArgsConstructor
     @AllArgsConstructor
     public static class OppfolgingData {
-        public OppfolgingData(OppfolgingDataFraFeed feedelement) {
-            this.feedelement = feedelement;
-        }
-
         public OppfolgingDataFraFeed feedelement;
         public String meldingsName;
     }
---
+++ src/main/java/no/nav/fo/veilarbdirigent/output/services/VeilarbregisteringService.java
@@ -0,0 +1,48 @@
+package no.nav.fo.veilarbdirigent.output.services;
+
+import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.apiapp.util.UrlUtils;
+import no.nav.dialogarena.aktor.AktorService;
+import no.nav.fo.veilarbdirigent.output.domain.OrdinaerBrukerRegistrering;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import java.util.function.Supplier;
+
+import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
+
+@Service
+@Slf4j
+public class VeilarbregisteringService {
+    public static final String VEILARBREGISTRERINGAPI_URL_PROPERTY = "VEILARBREGISTRERINGAPI_URL";
+    private final String host;
+
+    @Inject
+    private Client client;
+
+    @Inject
+    private AktorService aktorService;
+
+    public VeilarbregisteringService() {
+        Supplier<String> naisUrl = () -> UrlUtils.clusterUrlForApplication("veilarbregistrering") + "/veilarbregistrering/api";
+        this.host = getOptionalProperty(VEILARBREGISTRERINGAPI_URL_PROPERTY).orElseGet(naisUrl);
+    }
+
+    public Try<OrdinaerBrukerRegistrering> hentRegistrering(String aktorId) {
+        return Try.of(() -> {
+            String fnr = aktorService.getFnr(aktorId).get();
+            String url = String.format("%s/registrering?fnr=%s", host, fnr);
+            return client.target(url).request().get();
+        })
+                .filter((resp) -> resp.getStatus() >= 200 && resp.getStatus() < 300)
+                .map((resp) -> {
+                    if (resp.getStatus() == 204){
+                        return null;
+                    }
+                    return resp.readEntity(OrdinaerBrukerRegistrering.class);
+                })
+                .onFailure((error) -> log.warn("Fail to request registrering: " + error));
+    }
+}

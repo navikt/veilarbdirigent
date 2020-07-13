bfb5fc0a64311beb5a698a1954f6841cdaf1060a
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-07-13 11:55:05 +0200

    Fix bean annotation v3

--- src/main/java/no/nav/veilarbdirigent/input/InputConfig.java
+++ src/main/java/no/nav/veilarbdirigent/input/InputConfig.java
@@ -1,13 +1,11 @@
 package no.nav.veilarbdirigent.input;
 
-import no.nav.veilarbdirigent.input.rest.RestConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
         FeedConfig.class,
-        RestConfig.class
 })
 public class InputConfig {
 }
--- src/main/java/no/nav/veilarbdirigent/input/rest/BusyController.java
+++ src/main/java/no/nav/veilarbdirigent/input/rest/BusyController.java
@@ -1,18 +1,17 @@
 package no.nav.veilarbdirigent.input.rest;
 
 import no.nav.veilarbdirigent.core.Core;
-import org.springframework.stereotype.Service;
-
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
    This is a service to create busy tasks. These do nothing and simply validates that the internal flow works
    as expected
  */
 
-@Service
-@Path("/busy")
+@RestController
+@RequestMapping("/busy")
 public class BusyController {
 
     private final Core core;
@@ -21,8 +20,7 @@ public class BusyController {
         this.core = core;
     }
 
-    @POST
-    @Path("/new")
+    @PostMapping("/new")
     public String makeBusyTask() {
         BusyMessage msg = new BusyMessage();
         core.submit(msg);

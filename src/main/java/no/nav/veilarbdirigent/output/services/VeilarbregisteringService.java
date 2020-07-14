790663978285ab8aa849ef2bb5b11916ef2b8c44
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-07-14 14:50:37 +0200

    Try to get reg info

--- src/main/java/no/nav/veilarbdirigent/output/services/VeilarbregisteringService.java
+++ src/main/java/no/nav/veilarbdirigent/output/services/VeilarbregisteringService.java
@@ -3,23 +3,21 @@ package no.nav.veilarbdirigent.output.services;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.client.aktorregister.AktorregisterClient;
+import no.nav.common.rest.client.RestUtils;
 import no.nav.common.utils.UrlUtils;
 import no.nav.veilarbdirigent.output.domain.BrukerRegistreringWrapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import org.slf4j.Logger;
+import okhttp3.Response;
 
 import java.util.function.Supplier;
 
 import static no.nav.common.utils.EnvironmentUtils.getOptionalProperty;
-import static no.nav.veilarbdirigent.utils.SerializerUtils.mapper;
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Slf4j
 public class VeilarbregisteringService {
     public static final String VEILARBREGISTRERINGAPI_URL_PROPERTY = "VEILARBREGISTRERINGAPI_URL";
     private final String host;
-    private static final Logger LOG = getLogger(VeilarbregisteringService.class);
 
     private final OkHttpClient client;
     private final AktorregisterClient aktorService;
@@ -39,14 +37,13 @@ public class VeilarbregisteringService {
                 .url(url)
                 .build();
 
-        try{
-            var resp = client.newCall(request).execute();
-            var body = resp.body().string();
+        try (Response response = client.newCall(request).execute()) {
+            RestUtils.throwIfNotSuccessful(response);
 
-            LOG.warn("reg info: " + body);
-            return Try.success(mapper.readValue(body, BrukerRegistreringWrapper.class));
-        }
-        catch (Exception e){
+            BrukerRegistreringWrapper entity = RestUtils.parseJsonResponse(response, BrukerRegistreringWrapper.class).get();
+            return Try.success(entity);
+
+        } catch (Exception e){
             log.warn("Fail request to registrering: " + e);
             return Try.failure(e);
         }

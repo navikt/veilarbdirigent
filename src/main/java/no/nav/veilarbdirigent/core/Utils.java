d0c2890a846ee3019b6a757468b429855ce5ad26
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-07-14 14:30:36 +0200

    Sett correct MDCConstants.MDC_CALL_ID

--- src/main/java/no/nav/veilarbdirigent/core/Utils.java
+++ src/main/java/no/nav/veilarbdirigent/core/Utils.java
@@ -1,5 +1,6 @@
 package no.nav.veilarbdirigent.core;
 
+import no.nav.common.log.MDCConstants;
 import org.slf4j.MDC;
 
 public class Utils {
@@ -7,10 +8,12 @@ public class Utils {
     public static void runInMappedDiagnosticContext(String key, String value, Runnable func){
         try {
             MDC.put(key,value);
+            MDC.put(MDCConstants.MDC_CALL_ID, value);
             func.run();
         }
         finally {
             MDC.remove(key);
+            MDC.remove(MDCConstants.MDC_CALL_ID);
         }
 
     }

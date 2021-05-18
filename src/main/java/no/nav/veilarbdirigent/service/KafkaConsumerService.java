e3d6df67cde400c7a3eb88d2837e31537a6bb2e6
Alexander Gård <alex.gaard@hotmail.com>
2021-05-18 15:34:20 +0200

    Utfør tasks umiddelbart når kafka melding prosesseres

--- src/main/java/no/nav/veilarbdirigent/service/KafkaConsumerService.java
+++ src/main/java/no/nav/veilarbdirigent/service/KafkaConsumerService.java
@@ -1,5 +1,6 @@
 package no.nav.veilarbdirigent.service;
 
+import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
@@ -12,8 +13,10 @@ import no.nav.veilarbdirigent.client.veilarbregistrering.domain.BrukerRegistreri
 import no.nav.veilarbdirigent.domain.OppfolgingStartetKafkaDTO;
 import no.nav.veilarbdirigent.repository.TaskRepository;
 import no.nav.veilarbdirigent.repository.domain.Task;
+import no.nav.veilarbdirigent.utils.DbUtils;
 import no.nav.veilarbdirigent.utils.OppfolgingsperiodeUtils;
 import no.nav.veilarbdirigent.utils.RegistreringUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -24,6 +27,7 @@ import java.util.function.Function;
 
 import static no.nav.veilarbdirigent.utils.TaskFactory.*;
 import static no.nav.veilarbdirigent.utils.TaskUtils.createTaskIfNotStoredInDb;
+import static no.nav.veilarbdirigent.utils.TaskUtils.getStatusFromTry;
 
 @Slf4j
 @Service
@@ -38,11 +42,25 @@ public class KafkaConsumerService {
 
     private final UnleashService unleashService;
 
+    private final TaskProcessorService taskProcessorService;
+
     private final TaskRepository taskRepository;
+
+    private final JdbcTemplate jdbcTemplate;
 
     public void behandleOppfolgingStartet(OppfolgingStartetKafkaDTO oppfolgingStartetKafkaDTO) {
         if (!unleashService.isKafkaEnabled()) {
-            throw new RuntimeException("Kafka toggle is not enabled");
+            log.info("Kafka toggle is not enabled, skipping processing of record for aktorId={}", oppfolgingStartetKafkaDTO.getAktorId());
+            return;
+        }
+
+        /*
+            Siden vi utfører oppgaver som ikke er idempotent før vi lagrer resultatet i databasen, så gjør vi en ekstra sjekk
+            på om koblingen til databasen er grei, slik at vi ikke utfører oppgaver og ikke får lagret resultatet.
+        */
+        if (DbUtils.checkDbHealth(jdbcTemplate).isUnhealthy()) {
+            log.error("Health check failed, aborting consumption of kafka record");
+            throw new IllegalStateException("Cannot connect to database");
         }
 
         AktorId aktorId = oppfolgingStartetKafkaDTO.getAktorId();
@@ -68,14 +86,21 @@ public class KafkaConsumerService {
         boolean erNyRegistrert = RegistreringUtils.erNyregistrert(brukerRegistrering);
         boolean erNySykmeldtBrukerRegistrert = RegistreringUtils.erNySykmeldtBrukerRegistrert(brukerRegistrering);
 
-        List<Task> tasksToStore = new ArrayList<>();
+        List<Task> tasksToPerform = new ArrayList<>();
 
         if (erNyRegistrert) {
             Optional<Task> maybePermittertDialogTask = createTaskIfNotStoredInDb(
                     () -> lagKanskjePermittertDialogTask(oppfolgingsperiodeId, aktorId), taskRepository
             );
 
-            maybePermittertDialogTask.ifPresent(tasksToStore::add);
+            if (maybePermittertDialogTask.isPresent()) {
+                Task permittertDialogTask = maybePermittertDialogTask.get();
+
+                Try<String> dialogTaskResult = taskProcessorService.processOpprettDialogTask(permittertDialogTask);
+                permittertDialogTask.setTaskStatus(getStatusFromTry(dialogTaskResult));
+
+                tasksToPerform.add(permittertDialogTask);
+            }
         }
 
         if (erNySykmeldtBrukerRegistrert || erNyRegistrert) {
@@ -87,11 +112,29 @@ public class KafkaConsumerService {
                     () -> lagJobbsokerkompetanseAktivitetTask(oppfolgingsperiodeId, aktorId), taskRepository
             );
 
-            maybeCvJobbprofilAktivitetTask.ifPresent(tasksToStore::add);
-            maybeJobbsokerkompetanseAktivitetTask.ifPresent(tasksToStore::add);
+
+            if (maybeCvJobbprofilAktivitetTask.isPresent()) {
+                Task cvJobbprofilAktivitetTask = maybeCvJobbprofilAktivitetTask.get();
+
+                Try<String> cvJobbprofilAktivitetResult = taskProcessorService.processOpprettAktivitetTask(cvJobbprofilAktivitetTask);
+                cvJobbprofilAktivitetTask.setTaskStatus(getStatusFromTry(cvJobbprofilAktivitetResult));
+
+                tasksToPerform.add(cvJobbprofilAktivitetTask);
+            }
+
+            if (maybeJobbsokerkompetanseAktivitetTask.isPresent()) {
+                Task jobbsokerkompetanseAktivitetTask = maybeJobbsokerkompetanseAktivitetTask.get();
+
+                Try<String> jobbsokerkompetanseAktivitetResult = taskProcessorService.processOpprettAktivitetTask(jobbsokerkompetanseAktivitetTask);
+                jobbsokerkompetanseAktivitetTask.setTaskStatus(getStatusFromTry(jobbsokerkompetanseAktivitetResult));
+
+                tasksToPerform.add(jobbsokerkompetanseAktivitetTask);
+            }
         }
 
-        taskRepository.insert(tasksToStore);
+        taskRepository.insert(tasksToPerform);
+
+        log.info("Finished consuming kafka record for aktorId={}", aktorId);
     }
 
 }

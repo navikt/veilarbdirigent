f524f78a07dcdfe42097a71e81336b38a8f5225f
marcjas <marc.joseph.ablian.saputil@nav.no>
2021-10-22 15:46:43 +0200

    bump commons

--- src/main/java/no/nav/veilarbdirigent/config/KafkaConfig.java
+++ src/main/java/no/nav/veilarbdirigent/config/KafkaConfig.java
@@ -2,30 +2,23 @@ package no.nav.veilarbdirigent.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import net.javacrumbs.shedlock.core.LockProvider;
+import no.nav.common.featuretoggle.UnleashClient;
 import no.nav.common.kafka.consumer.KafkaConsumerClient;
-import no.nav.common.kafka.consumer.TopicConsumer;
 import no.nav.common.kafka.consumer.feilhandtering.KafkaConsumerRecordProcessor;
 import no.nav.common.kafka.consumer.feilhandtering.KafkaConsumerRepository;
-import no.nav.common.kafka.consumer.feilhandtering.OracleConsumerRepository;
-import no.nav.common.kafka.consumer.feilhandtering.StoredRecordConsumer;
 import no.nav.common.kafka.consumer.feilhandtering.util.KafkaConsumerRecordProcessorBuilder;
-import no.nav.common.kafka.consumer.util.ConsumerUtils;
 import no.nav.common.kafka.consumer.util.KafkaConsumerClientBuilder;
+import no.nav.common.kafka.consumer.util.TopicConsumerConfig;
+import no.nav.common.kafka.spring.OracleJdbcTemplateConsumerRepository;
 import no.nav.common.utils.Credentials;
-import no.nav.veilarbdirigent.domain.OppfolgingStartetKafkaDTO;
-import no.nav.veilarbdirigent.service.KafkaConsumerService;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.util.Map;
+import java.util.List;
 
-import static no.nav.common.kafka.consumer.util.ConsumerUtils.jsonConsumer;
 import static no.nav.common.kafka.util.KafkaPropertiesPreset.onPremDefaultConsumerProperties;
 
 @Configuration
@@ -33,71 +26,61 @@ import static no.nav.common.kafka.util.KafkaPropertiesPreset.onPremDefaultConsum
 public class KafkaConfig {
 
     public final static String CONSUMER_GROUP_ID = "veilarbdirigent-consumer";
-
-    @Autowired
-    KafkaConsumerClient<String, String> consumerClient;
+    private static final String ONPREM_KAFKA_DISABLED = "veilarbdirigent.kafka.onprem.consumer.disabled";
 
     @Autowired
     KafkaConsumerRecordProcessor consumerRecordProcessor;
 
     @Bean
-    public KafkaConsumerRepository kafkaConsumerRepository(DataSource dataSource) {
-        return new OracleConsumerRepository(dataSource);
+    public KafkaConsumerRepository kafkaConsumerRepository(JdbcTemplate jdbcTemplate) {
+        return new OracleJdbcTemplateConsumerRepository(jdbcTemplate);
     }
 
     @Bean
-    public Map<String, TopicConsumer<String, String>> topicConsumers(
-            KafkaConsumerService kafkaConsumerService,
-            KafkaProperties kafkaProperties
-    ) {
-        return Map.of(
-                kafkaProperties.getOppfolgingStartetTopic(),
-                jsonConsumer(OppfolgingStartetKafkaDTO.class, kafkaConsumerService::behandleOppfolgingStartet)
-        );
-    }
-
-    @Bean
-    public KafkaConsumerClient<String, String> consumerClient(
-            Map<String, TopicConsumer<String, String>> topicConsumers,
-            KafkaConsumerRepository kafkaConsumerRepository,
+    public KafkaConsumerClient consumerClient(
+            List<TopicConsumerConfig<?, ?>> topicConfigs,
             Credentials credentials,
             KafkaProperties kafkaProperties,
-            MeterRegistry meterRegistry
+            MeterRegistry meterRegistry,
+            UnleashClient unleashClient,
+            KafkaConsumerRepository kafkaConsumerRepository
     ) {
-        return KafkaConsumerClientBuilder.<String, String>builder()
+        var clientBuilder = KafkaConsumerClientBuilder.builder()
                 .withProperties(onPremDefaultConsumerProperties(CONSUMER_GROUP_ID, kafkaProperties.getBrokersUrl(), credentials))
-                .withRepository(kafkaConsumerRepository)
-                .withSerializers(new StringSerializer(), new StringSerializer())
-                .withStoreOnFailureConsumers(topicConsumers)
-                .withMetrics(meterRegistry)
-                .withLogging()
-                .build();
+                .withToggle(() -> unleashClient.isEnabled(ONPREM_KAFKA_DISABLED));
+
+        topicConfigs.forEach(it -> {
+            clientBuilder.withTopicConfig(
+                    new KafkaConsumerClientBuilder
+                            .TopicConfig()
+                            .withConsumerConfig(it)
+                            .withMetrics(meterRegistry)
+                            .withLogging()
+                            .withStoreOnFailure(kafkaConsumerRepository)
+            );
+        });
+
+        var client = clientBuilder.build();
+        client.start();
+
+        return client;
     }
 
     @Bean
     public KafkaConsumerRecordProcessor consumerRecordProcessor(
+            List<TopicConsumerConfig<?, ?>> topicConfigs,
             LockProvider lockProvider,
-            KafkaConsumerRepository kafkaConsumerRepository,
-            Map<String, TopicConsumer<String, String>> topicConsumers
+            KafkaConsumerRepository kafkaConsumerRepository
     ) {
-        Map<String, StoredRecordConsumer> storedRecordConsumers = ConsumerUtils.toStoredRecordConsumerMap(
-                topicConsumers,
-                new StringDeserializer(),
-                new StringDeserializer()
-        );
-
-        return KafkaConsumerRecordProcessorBuilder
+        KafkaConsumerRecordProcessor kafkaConsumerRecordProcessor = KafkaConsumerRecordProcessorBuilder
                 .builder()
                 .withLockProvider(lockProvider)
                 .withKafkaConsumerRepository(kafkaConsumerRepository)
-                .withRecordConsumers(storedRecordConsumers)
+                .withConsumerConfigs(topicConfigs)
                 .build();
-    }
 
+        kafkaConsumerRecordProcessor.start();
 
-    @PostConstruct
-    public void start() {
-        consumerClient.start();
-        consumerRecordProcessor.start();
+        return kafkaConsumerRecordProcessor;
     }
 }

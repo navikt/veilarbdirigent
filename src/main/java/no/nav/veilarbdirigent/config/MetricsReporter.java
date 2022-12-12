12bbf482a5459ae5ae3a596ec1d748a478ddb187
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-12-12 20:10:15 +0100

    Flytte til Kafka Aiven

---
+++ src/main/java/no/nav/veilarbdirigent/config/MetricsReporter.java
@@ -0,0 +1,32 @@
+package no.nav.veilarbdirigent.config;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static io.micrometer.prometheus.PrometheusConfig.DEFAULT;
+
+@Slf4j
+@Component
+public class MetricsReporter {
+
+    private static final MeterRegistry prometheusMeterRegistry = new ProtectedPrometheusMeterRegistry();
+
+    public static MeterRegistry getMeterRegistry() {
+        return prometheusMeterRegistry;
+    }
+
+    public static class ProtectedPrometheusMeterRegistry extends PrometheusMeterRegistry {
+        public ProtectedPrometheusMeterRegistry() {
+            super(DEFAULT);
+        }
+
+        @Override
+        public void close() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}

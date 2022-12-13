2ccb2355b74c64f30ab4800159d1d5abb76fc29b
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-12-13 12:50:50 +0100

    Bruk av AzureAD for veilarboppfolging
    Update nais.yaml
    
    Update nais-q.yaml
    
    Update src/main/java/no/nav/veilarbdirigent/config/FilterConfig.java
    
    Update nais-q.yaml
    
    Update nais.yaml
    
    Bump fra aktorregister til pdl
    
    La til junit-vintage-engine for å kjøre junit4 tester
    
    Ta i bruk veilarbregistrering i gcp i dev
    
    veilarbregistrering er fortsatt ikke klar til bruk i gcp i prod, så bruker prod-fss-appen inntil videre.
    
    Bugfix
    
    Debug
    
    Debug
    
    Changes based on code review

--- src/main/java/no/nav/veilarbdirigent/config/FilterConfig.java
+++ src/main/java/no/nav/veilarbdirigent/config/FilterConfig.java
@@ -4,8 +4,10 @@ import no.nav.common.auth.context.UserRole;
 import no.nav.common.auth.oidc.filter.OidcAuthenticationFilter;
 import no.nav.common.auth.oidc.filter.OidcAuthenticatorConfig;
 import no.nav.common.auth.utils.UserTokenFinder;
-import no.nav.common.log.LogFilter;
+import no.nav.common.rest.filter.LogRequestFilter;
 import no.nav.common.rest.filter.SetStandardHttpHeadersFilter;
+import no.nav.common.token_client.builder.AzureAdTokenClientBuilder;
+import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,8 +43,8 @@ public class FilterConfig {
 
     @Bean
     public FilterRegistrationBean logFilterRegistrationBean() {
-        FilterRegistrationBean<LogFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new LogFilter(requireApplicationName(), isDevelopment().orElse(false)));
+        FilterRegistrationBean<LogRequestFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new LogRequestFilter(requireApplicationName(), isDevelopment().orElse(false)));
         registration.setOrder(1);
         registration.addUrlPatterns("/*");
         return registration;
@@ -73,4 +75,5 @@ public class FilterConfig {
         return registration;
     }
 
+ 
 }

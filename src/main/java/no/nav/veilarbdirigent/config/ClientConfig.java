f170f87f2583eaecc6b78b7712bd3e0ab125a1fb
sigurdgroneng <sigurd.groneng@nav.no>
2023-01-10 14:43:48 +0100

    Add missing contextpath for veilarbregistrering api-url

--- src/main/java/no/nav/veilarbdirigent/config/ClientConfig.java
+++ src/main/java/no/nav/veilarbdirigent/config/ClientConfig.java
@@ -66,7 +66,7 @@ public class ClientConfig {
         var appName = "veilarbregistrering";
         String url = isDevelopment().orElse(false)
                 ? joinPaths(createDevInternalIngressUrl(appName), appName)
-                : createProdInternalIngressUrl(appName);
+                : joinPaths(createProdInternalIngressUrl(appName), appName);
         var cluster = isDevelopment().orElse(false) ? "dev-gcp" : "prod-fss";
         return new VeilarbregistreringClientImpl(
                 url,

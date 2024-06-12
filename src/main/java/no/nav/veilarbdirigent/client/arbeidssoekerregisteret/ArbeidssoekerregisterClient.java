cf2e413700edaa555674f8438b0a6d2cb170f7bb
johannetronstad <johanne.tronstad@nav.no>
2024-06-12 13:05:18 +0200

    Implementer hentProfileringer

--- src/main/java/no/nav/veilarbdirigent/client/arbeidssoekerregisteret/ArbeidssoekerregisterClient.java
+++ src/main/java/no/nav/veilarbdirigent/client/arbeidssoekerregisteret/ArbeidssoekerregisterClient.java
@@ -12,6 +12,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -51,13 +52,31 @@ public class ArbeidssoekerregisterClient {
             return RestUtils.parseJsonResponseArrayOrThrow(response, ArbeidssoekerPeriodeResponse.class);
         }
         catch (Exception e){
-            log.error("Error hent oppfolgingsperiode " + e);
+            log.error("Error hent arbeidssøkerperioder " + e);
             throw e;
         }
     }
 
-    public ProfileringsResultat hentProfilering(ProfileringRequest request) {
-        return null;
+    @SneakyThrows
+    public List<ProfileringResponse> hentProfileringer(Fnr fnr, UUID arbeidssøkerperiodeId) {
+        String url = UrlUtils.joinPaths(apiUrl, "/api/v1/veileder/profilering");
+        var body = JsonUtils.toJson(new ProfileringRequest(fnr.get(), arbeidssøkerperiodeId));
+        log.info("Hent profileringer");
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", "Bearer " + machineToMachineTokenSupplier.get())
+                .post(RequestBody.create(MEDIA_TYPE_JSON, body))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            RestUtils.throwIfNotSuccessful(response);
+            return RestUtils.parseJsonResponseArrayOrThrow(response, ProfileringResponse.class);
+        }
+        catch (Exception e){
+            log.error("Error hent profileringer " + e);
+            throw e;
+        }
     }
 
     static class ArbeidssoekerPeriodeResponse {
@@ -68,14 +87,25 @@ public class ArbeidssoekerregisterClient {
 
     static class MetadataResponse {
         ZonedDateTime tidspunkt;
-        BrukerResponse utfoertAv;
+        UtførtAv utfoertAv;
         String kilde;
         String aarsak;
-        TispunktFraKildeResponse tidspunktFraKilde;
-
+        TidspunktFraKildeResponse tidspunktFraKilde;
     }
 
-    static class BrukerResponse {
+    static class ProfileringResponse {
+        String profileringId;
+        String periodeId;
+        ProfileringSendtInnAv profileringSendtInnAv;
+        ProfileringsResultat profilertTil;
+    }
+
+    static class ProfileringSendtInnAv {
+        LocalDateTime tidspunkt;
+        UtførtAv utfoertAv;
+    }
+
+    static class UtførtAv {
         BrukerType type;
         String id;
     }
@@ -84,7 +114,7 @@ public class ArbeidssoekerregisterClient {
 
     record ArbeidssoekerperiodeRequest(String identitetsnummer) { }
 
-    static class TispunktFraKildeResponse {
+    static class TidspunktFraKildeResponse {
         ZonedDateTime tidspunkt;
         AvviksTypeResponse avviksType;
     }
--- src/test/java/no/nav/veilarbdirigent/client/arbeidssoekerregisteret/ArbeidssoekerregisterClientTest.java
+++ src/test/java/no/nav/veilarbdirigent/client/arbeidssoekerregisteret/ArbeidssoekerregisterClientTest.java
@@ -1,15 +1,14 @@
 package no.nav.veilarbdirigent.client.arbeidssoekerregisteret;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import no.nav.common.json.JsonUtils;
 import no.nav.common.types.identer.Fnr;
 import no.nav.veilarbdirigent.TestUtils;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +24,18 @@ class ArbeidssoekerregisterClientTest {
         Fnr fnr = Fnr.of("1234");
         mockAvArbeidssøkerperioder(fnr.get());
         var arbeidsøkerPerioder = arbeidssoekerregisterClient.hentArbeidsoekerPerioder(fnr);
-        assertThat(arbeidsøkerPerioder).hasSize(2);
+        assertThat(arbeidsøkerPerioder).hasSize(1);
+    }
+
+    @Test
+    void skalKunneHenteProfileringer() {
+        Fnr fnr = Fnr.of("1234");
+        UUID arbeidssøkerperiodeId = UUID.randomUUID();
+        mockAvProfilering(fnr.get(), arbeidssøkerperiodeId);
+
+        var profileringer = arbeidssoekerregisterClient.hentProfileringer(fnr, arbeidssøkerperiodeId);
+
+        assertThat(profileringer).hasSize(1);
     }
 
     @Test
@@ -36,6 +46,7 @@ class ArbeidssoekerregisterClientTest {
     }
 
     private void mockAvArbeidssøkerperioder(String fnr) {
+        String jsonResponse = TestUtils.readTestResourceFile("client/arbeidssoekerregisteret/arbeidssoekerperioder.json");
         givenThat(post(urlEqualTo("/api/v1/veileder/arbeidssoekerperioder"))
                 .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer TOKEN"))
                 .withRequestBody(equalToJson(String.format("""
@@ -47,69 +58,26 @@ class ArbeidssoekerregisterClientTest {
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                        .withBody("""
-                            [
-                              {
-                                "periodeId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                                "startet": {
-                                  "tidspunkt": "2021-09-29T11:22:33.444Z",
-                                  "utfoertAv": {
-                                    "type": "UKJENT_VERDI",
-                                    "id": "12345678910"
-                                  },
-                                  "kilde": "string",
-                                  "aarsak": "string",
-                                  "tidspunktFraKilde": {
-                                    "tidspunkt": "2021-09-29T11:20:33.444Z",
-                                    "avviksType": "UKJENT_VERDI"
-                                  }
-                                },
-                                "avsluttet": {
-                                  "tidspunkt": "2021-09-29T11:22:33.444Z",
-                                  "utfoertAv": {
-                                    "type": "UKJENT_VERDI",
-                                    "id": "12345678910"
-                                  },
-                                  "kilde": "string",
-                                  "aarsak": "string",
-                                  "tidspunktFraKilde": {
-                                    "tidspunkt": "2021-09-29T11:20:33.444Z",
-                                    "avviksType": "UKJENT_VERDI"
-                                  }
-                                }
-                              },
-                              {
-                                "periodeId": "2fa85f64-5717-4562-b3fc-2c963f66afa6",
-                                "startet": {
-                                  "tidspunkt": "2023-09-29T11:22:33.444Z",
-                                  "utfoertAv": {
-                                    "type": "UKJENT_VERDI",
-                                    "id": "12345678910"
-                                  },
-                                  "kilde": "string",
-                                  "aarsak": "string",
-                                  "tidspunktFraKilde": {
-                                    "tidspunkt": "2023-09-29T11:20:33.444Z",
-                                    "avviksType": "UKJENT_VERDI"
-                                  }
-                                },
-                                "avsluttet": {
-                                  "tidspunkt": "2024-09-29T11:22:33.444Z",
-                                  "utfoertAv": {
-                                    "type": "UKJENT_VERDI",
-                                    "id": "12345678910"
-                                  },
-                                  "kilde": "string",
-                                  "aarsak": "string",
-                                  "tidspunktFraKilde": {
-                                    "tidspunkt": "2024-09-29T11:20:33.444Z",
-                                    "avviksType": "UKJENT_VERDI"
-                                  }
-                                }
-                              }
-                            ]
-                        """)
+                        .withBody(jsonResponse)
                 )
         );
+    }
+
+    private void mockAvProfilering(String fnr, UUID arbeidssøkerperiode) {
+        String jsonResponse = TestUtils.readTestResourceFile("client/arbeidssoekerregisteret/profileringer.json");
+        givenThat(post(urlEqualTo("/api/v1/veileder/profilering"))
+                .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer TOKEN"))
+                .withRequestBody(equalToJson(String.format("""
+                        {
+                           "identitetsnummer": "%s",
+                           "periodeId": "%s"
+                         }
+                        """, fnr, arbeidssøkerperiode)
+                ))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .withBody(jsonResponse))
+                );
     }
 }

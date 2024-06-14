2b8c8fe354d24b3b2e3dc424b1e8afb49576da22
Mads Lee Giil <mads.lee.giil@nav.no>
2024-06-14 14:51:41 +0200

    Fjern ubrukt kode for opprettelse av dialog

--- src/main/java/no/nav/veilarbdirigent/utils/TaskFactory.java
+++ src/main/java/no/nav/veilarbdirigent/utils/TaskFactory.java
@@ -2,7 +2,6 @@ package no.nav.veilarbdirigent.utils;
 
 import no.nav.common.types.identer.AktorId;
 import no.nav.veilarbdirigent.repository.domain.OpprettAktivitetTaskDataV2;
-import no.nav.veilarbdirigent.repository.domain.OpprettDialogTaskDataV2;
 import no.nav.veilarbdirigent.repository.domain.Task;
 import no.nav.veilarbdirigent.repository.domain.TaskType;
 
@@ -14,21 +13,9 @@ public class TaskFactory {
 
     private final static String CV_JOBBPROFIL_AKTIVITET_TASK_ID_SUFFIX = "cv_jobbprofil_aktivitet";
 
-    private final static String KANSKJE_PERMITTERT_DIALOG_TASK_ID_SUFFIX = "kanskjePermitert";
-
     private final static String CV_JOBBPROFIL_AKTIVITET_MAL = "cv_jobbprofil_aktivitet";
 
-    public final static TaskType AKTIVITET_TASK_TYPE_V1 = TaskType.of("OPPFOLGING_OPPRETT_AKTIVITET");
-
-    public final static TaskType DIALOG_TASK_TYPE_V1 = TaskType.of("OPPFOLGING_OPPRETT_DIALOG");
-
     public final static TaskType AKTIVITET_TASK_TYPE_V2 = TaskType.of("OPPFOLGING_OPPRETT_AKTIVITET_v2");
-
-    public final static TaskType DIALOG_TASK_TYPE_V2 = TaskType.of("OPPFOLGING_OPPRETT_DIALOG_v2");
-
-
-    private final static String KANSKJE_PERMITTERT_DIALOG_NAME = "kanskje_permitert_dialog";
-
 
     public static Task lagCvJobbprofilAktivitetTask(UUID oppfolgingsPeriodeId, AktorId aktorId) {
         return new Task()
@@ -36,12 +23,4 @@ public class TaskFactory {
                 .withType(AKTIVITET_TASK_TYPE_V2)
                 .withJsonData(toJson(new OpprettAktivitetTaskDataV2(aktorId, CV_JOBBPROFIL_AKTIVITET_MAL)));
     }
-
-    public static Task lagKanskjePermittertDialogTask(String id, AktorId aktorId) {
-        return new Task()
-                .withId(id + "_" + KANSKJE_PERMITTERT_DIALOG_TASK_ID_SUFFIX)
-                .withType(DIALOG_TASK_TYPE_V2)
-                .withJsonData(toJson(new OpprettDialogTaskDataV2(aktorId, KANSKJE_PERMITTERT_DIALOG_NAME)));
-    }
-
 }

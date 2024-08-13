5a8f7806171cab7f44220921a4acddfa80458015
Sigurd Grøneng <sigurdgroneng@gmail.com>
2024-08-13 08:39:29 +0200

    Fjern ubrukt kode for opprettelse av dialog (#167)
    * Slett ubrukt klasse
    
    * Hent profilering via endepunkt med samlet informasjon fra nytt arbeidssøkerregister (#164)
    
    * WIP: Legg til kall for samlet-informasjon til klienten
    
    * Gjør ferdig oppsett av nytt kall til samletInformasjon
    
    * Implementert samlet informasjon
    
    * Tilpass testene ny implementasjon
    
    * Rydd opp
    
    * Bruk siste parameter i samlet-informasjon
    
    * Tilpass tester nytt grensesnitt for klienten
    
    * Fiks optional variabel
    
    * Kort ned nevn på testfiler
    
    * Test github checks
    
    ---------
    
    Co-authored-by: johannetronstad <johanne.tronstad@nav.no>
    Co-authored-by: Johanne Tronstad <43403066+johatr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mads Lee Giil <mads.lee.giil@nav.no>
    Co-authored-by: Mads Lee Giil <74543378+madsleegiil@users.noreply.github.com>
    Co-authored-by: johannetronstad <johanne.tronstad@nav.no>
    Co-authored-by: Johanne Tronstad <43403066+johatr@users.noreply.github.com>

--- src/main/java/no/nav/veilarbdirigent/service/TaskProcessorService.java
+++ src/main/java/no/nav/veilarbdirigent/service/TaskProcessorService.java
@@ -4,14 +4,12 @@ import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.veilarbdirigent.repository.domain.OpprettAktivitetTaskDataV2;
-import no.nav.veilarbdirigent.repository.domain.OpprettDialogTaskDataV2;
 import no.nav.veilarbdirigent.repository.domain.Task;
 import no.nav.veilarbdirigent.repository.domain.TaskType;
 import org.springframework.stereotype.Service;
 
 import static no.nav.common.json.JsonUtils.fromJson;
 import static no.nav.veilarbdirigent.utils.TaskFactory.AKTIVITET_TASK_TYPE_V2;
-import static no.nav.veilarbdirigent.utils.TaskFactory.DIALOG_TASK_TYPE_V2;
 
 @Slf4j
 @Service
@@ -20,14 +18,10 @@ public class TaskProcessorService {
 
     private final AktivitetService aktivitetService;
 
-    private final DialogService dialogService;
-
     public Try<String> processTask(Task task) {
         TaskType taskType = task.getType();
         if (AKTIVITET_TASK_TYPE_V2.equals(taskType)) {
             return processOpprettAktivitetTask(task);
-        } else if (DIALOG_TASK_TYPE_V2.equals(taskType)) {
-            return processOpprettDialogTask(task);
         } else {
             String message = "Unable to process task of type " + taskType;
             log.error(message);
@@ -45,24 +39,8 @@ public class TaskProcessorService {
         }
     }
 
-    public Try<String> processOpprettDialogTask(Task opprettDialogTaskV2) {
-        try {
-            OpprettDialogTaskDataV2 taskData = fromJson(opprettDialogTaskV2.getJsonData(), OpprettDialogTaskDataV2.class);
-            return processOpprettDialogTaskData(taskData);
-        } catch (Exception e) {
-            log.error("Failed to process " + DIALOG_TASK_TYPE_V2, e);
-            return Try.failure(e);
-        }
-    }
-
     private Try<String> processOpprettAktivitetTaskData(OpprettAktivitetTaskDataV2 taskData) {
         log.info("Processing task {} aktorId={} malName={}", AKTIVITET_TASK_TYPE_V2, taskData.getAktorId(), taskData.getMalName());
         return aktivitetService.opprettAktivitetForBrukerMedMal(taskData.getAktorId(), taskData.getMalName());
     }
-
-    private Try<String> processOpprettDialogTaskData(OpprettDialogTaskDataV2 taskData) {
-        log.info("Processing task {} aktorId={} dialogName={}", DIALOG_TASK_TYPE_V2, taskData.getAktorId(), taskData.getDialogName());
-        return dialogService.opprettDialogHvisBrukerErPermittert(taskData.getAktorId());
-    }
-
 }

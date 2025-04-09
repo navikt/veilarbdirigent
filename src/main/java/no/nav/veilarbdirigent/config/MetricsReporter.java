b26b47acd1ef4875bf2be0329c6aa05f41d2a1b1
johannetronstad <johanne.tronstad@nav.no>
2025-04-09 09:10:49 +0200

    Bump til java 21 + distroless

--- src/main/java/no/nav/veilarbdirigent/config/MetricsReporter.java
+++ src/main/java/no/nav/veilarbdirigent/config/MetricsReporter.java
@@ -1,11 +1,12 @@
 package no.nav.veilarbdirigent.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import static io.micrometer.prometheus.PrometheusConfig.DEFAULT;
+import static io.micrometer.prometheusmetrics.PrometheusConfig.DEFAULT;
+
 
 @Slf4j
 @Component
